211aa7995bec3eff541d5107c1a1a3435a713541
Jack Yang <honghao@wustl.edu>
2025-09-28 16:02:20 -0500

    Finish Q6

--- VG1A_YangJack/Assets/Code/SpaceShooter/GameController.cs
+++ VG1A_YangJack/Assets/Code/SpaceShooter/GameController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
+using TMPro;
 
 namespace SpaceShooter
 {
@@ -15,6 +16,10 @@ namespace SpaceShooter
         public Transform[] spawnPoints;
         public GameObject[] asteroidPrefabs;
         public GameObject explosionPrefab;
+        public TMP_Text textScore;
+        public TMP_Text textMoney;
+        public TMP_Text missileSpeedUpgradeText;
+        public TMP_Text bonusUpgradeText;
         
         // Configuration
         public float maxAsteroidDelay = 2f;
@@ -23,10 +28,20 @@ namespace SpaceShooter
         // State Tracking
         public float timeElapsed;
         public float asteroidDelay;
+        public int score;
+        public int money;
+        public float missileSpeed = 2f;
+        public float bonusMultiplier = 1f;
 
         void Start()
         {
             StartCoroutine("AsteroidSpawnTimer");
+            
+            score = 0;
+            money = 0;
+            
+            missileSpeedUpgradeText.text = "Missile Speed $" + Mathf.RoundToInt(missileSpeed * 25);
+            bonusUpgradeText.text = "Multiplier $" + Mathf.RoundToInt(100 * bonusMultiplier);
         }
         
         // Methods
@@ -43,6 +58,14 @@ namespace SpaceShooter
             // Computer Asteroid Delay
             float decreaseDelayOverTime = maxAsteroidDelay - ((asteroidDelay - minAsteroidDelay) / 30f * timeElapsed);
             asteroidDelay = Math.Clamp(decreaseDelayOverTime, minAsteroidDelay, maxAsteroidDelay);
+
+            UpdateDisplay();
+        }
+
+        void UpdateDisplay()
+        {
+            textScore.text = score.ToString();
+            textMoney.text = money.ToString();
         }
 
         void SpawnAsteroid()
@@ -67,6 +90,38 @@ namespace SpaceShooter
             
             // Repeat
             StartCoroutine("AsteroidSpawnTimer");
+        }
+
+        public void EarnPoints(int pointAmount)
+        {
+            score += Mathf.RoundToInt(pointAmount * bonusMultiplier);
+            money += Mathf.RoundToInt(pointAmount * bonusMultiplier);
+        }
+
+        public void UpgradeMissileSpeed()
+        {
+            int cost = Mathf.RoundToInt(missileSpeed * 25);
+            if (cost <= money)
+            {
+                money -= cost;
+
+                missileSpeed += 1f;
+                
+                missileSpeedUpgradeText.text = "Missile Speed $" + Mathf.RoundToInt(missileSpeed * 25);
+            }
+        }
+
+        public void UpgradeBonus()
+        {
+            int cost = Mathf.RoundToInt(100 * bonusMultiplier);
+            if (cost <= money)
+            {
+                money -= cost;
+                
+                bonusMultiplier += 1f;
+                
+                bonusUpgradeText.text = "Multiplier $" + Mathf.RoundToInt(100 * bonusMultiplier);
+            }
         }
     }
 }
--- VG1A_YangJack/Assets/Code/SpaceShooter/Projectile.cs
+++ VG1A_YangJack/Assets/Code/SpaceShooter/Projectile.cs
@@ -23,8 +23,8 @@ namespace SpaceShooter
         // Update is called once per frame
         void Update()
         {
-            float acceleration = 1f;
-            float maxSpeed = 2f;
+            float acceleration = GameController.instance.missileSpeed / 2f;
+            float maxSpeed = GameController.instance.missileSpeed;
             
             ChooseNearestTarget();
             if (target != null)
@@ -78,6 +78,8 @@ namespace SpaceShooter
                     Quaternion.identity
                 );
                 Destroy(explosion, 0.25f);
+                
+                GameController.instance.EarnPoints(10);
             }
         }
     }
